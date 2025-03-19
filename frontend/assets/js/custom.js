29239af508713043c165558c33ef08c792d00f98
Aydin324 <ayddin2004@gmail.com>
2025-03-19 11:36:16 +0100

    Encorporated template and modified to fit basic project structure

--- frontend/assets/js/custom.js
+++ frontend/assets/js/custom.js
@@ -1,22 +1,26 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   $("main#spapp > section").height($(document).height() - 60);
 
-  var app = $.spapp({pageNotFound : 'error_404'}); // initialize
+  var app = $.spapp({ pageNotFound: "error_404" }); // initialize
 
   // define routes
   app.route({
-    view: 'view_1',
-    onCreate: function() { $("#view_1").append($.now()+': Written on create<br/>'); },
-    onReady: function() { $("#view_1").append($.now()+': Written when ready<br/>'); }
+    view: "home",
+    onCreate: function () {},
+    onReady: function () {},
   });
-  app.route({view: 'view_2', load: 'view_2.html' });
+  app.route({ view: "products", load: "products.html" });
   app.route({
-    view: 'view_3', 
-    onCreate: function() { $("#view_3").append("I'm the third view"); }
+    view: "orders",
+    load: "orders.html",
+    onCreate: function () {},
+  });
+  app.route({
+    view: "reviews",
+    load: "reviews.html",
+    onCreate: function () {},
   });
 
   // run app
   app.run();
-
 });
