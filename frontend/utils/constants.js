b4b675dc0aab5d44dea3e7dec91a923898216593
Aydin324 <ayddin2004@gmail.com>
2025-06-06 15:58:17 +0200

    Modified setup files to serve frontend correctly

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,5 +1,7 @@
 let Constants = {
-  PROJECT_BASE_URL: "http://localhost/WebDevelopment25/backend/",
+  PROJECT_BASE_URL: window.location.hostname === 'localhost' 
+      ? "http://localhost/WebDevelopment25/backend/"
+      : "/api/",
   USER_ROLE: "user",
   ADMIN_ROLE: "admin",
 };
